2950eaa27f7d6d64af5fe7dd1badbbf3ad9156f3
admin <bunny.shankar@gmail.com>
2017-02-21 02:26:08 -0800

    changes to new

---
+++ src/main/java/com/shankar/ant/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.shankar.ant;
+
+public class HelloWorld{
+
+
+public static void main(String...args){
+System.out.println("Hello World!");
+System.out.println("My First Jenkins project");
+}
+
+}
